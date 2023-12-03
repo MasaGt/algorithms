b91327d01c8d6ce78bacd83d7e8acebd27cde57f
MasaGt <gachapin2236tx@gmail.com>
2023-12-04 12:58:35 +1300

    Format source code

--- Sort/QuickSort/quickSort.ts
+++ Sort/QuickSort/quickSort.ts
@@ -3,56 +3,51 @@
  * @param data ソート対象
  * @param start 配列の操作範囲の最初のインデックス
  * @param end 配列の操作範囲の末尾のインデックス
- * @returns 
+ * @returns
  */
-export function quickSort(data: number[],
-                   start: number,
-                   end: number): void
-{
-    //長さが1よりも長い配列
-    if (start <= end) {
+export function quickSort(data: number[], start: number, end: number): void {
+  //長さが1よりも長い配列
+  if (start <= end) {
+    // サーチ範囲を示すindexであるhead, tail
+    let head = start;
+    let tail = end;
+    let pivot: number = data[Math.floor((head + tail) / 2)];
 
-        // サーチ範囲を示すindexであるhead, tail
-        let head = start;
-        let tail = end;
-        let pivot: number = data[Math.floor((head + tail)/2)];        
+    //パーティショニング作成
+    while (true) {
+      //pivotより大きい値を見つける(左側に移すために)
+      while (data[head] < pivot) {
+        //最終的にheadはdata[head]=pivotで止まる
+        head++;
+      }
 
-        //パーティショニング作成
-        while (true) {
-            
-            //pivotより大きい値を見つける(左側に移すために)
-            while (data[head] < pivot) {
-                //最終的にheadはdata[head]=pivotで止まる
-                head++;
-            }
+      //pivotより小さい値を見つける(右側に移すために)
+      while (pivot < data[tail]) {
+        //最終的にtailはdata[tail]=pivotで止まる
+        tail--;
+      }
 
-            //pivotより小さい値を見つける(右側に移すために)
-            while (pivot < data[tail]) {
-                //最終的にtailはdata[tail]=pivotで止まる
-                tail--;
-            }
+      //範囲内を走査し終えたらbreak
+      if (head >= tail) {
+        break;
+      }
 
-            //範囲内を走査し終えたらbreak
-            if (head >= tail) {
-                break;
-            }
-
-            //swap
-            let temp: number = data[head];
-            data[head] = data[tail];
-            data[tail] = temp;
-            head++;
-            tail--;
-        }
-
-        //pivotより小さい値のグループで再帰
-        quickSort(data, start, head - 1);
-        //pivotより大きい値のグループで再帰
-        quickSort(data, tail + 1, end);
+      //swap
+      let temp: number = data[head];
+      data[head] = data[tail];
+      data[tail] = temp;
+      head++;
+      tail--;
     }
 
-    return;
-};
+    //pivotより小さい値のグループで再帰
+    quickSort(data, start, head - 1);
+    //pivotより大きい値のグループで再帰
+    quickSort(data, tail + 1, end);
+  }
+
+  return;
+}
 
 let data = [7, 6, 10, 2, 5, 1, 8, 3, 9, 4];
 quickSort(data, 0, data.length - 1);
--- Sort/SelectionSort/selectionSort.ts
+++ Sort/SelectionSort/selectionSort.ts
@@ -1,27 +1,26 @@
 /**
  * Sort data in descending order
- * @param data 
+ * @param data
  * @returns sorted data
  */
 const sort = (data: number[]): number[] => {
+  for (let i = 0; i < data.length; i++) {
+    let minValue = data[i];
+    let minIndex = i;
 
-    for (let i = 0; i < data.length; i++) {
-        let minValue = data[i];
-        let minIndex = i;
-
-        for (let k = i + 1; k < data.length; k++) {
-            if (data[k] < minValue) {
-                minValue = data[k];
-                minIndex = k;
-            }
-        }
-
-        // swap data so that min data comes forward
-        let temp = data[i];
-        data[i] = minValue;
-        data[minIndex] = temp;
+    for (let k = i + 1; k < data.length; k++) {
+      if (data[k] < minValue) {
+        minValue = data[k];
+        minIndex = k;
+      }
     }
-    return data;
+
+    // swap data so that min data comes forward
+    let temp = data[i];
+    data[i] = minValue;
+    data[minIndex] = temp;
+  }
+  return data;
 };
 
 console.log(sort([10, 1, 8, 7, 26, 55]));

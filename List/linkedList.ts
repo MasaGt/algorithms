863a0fad57744382c1362b9cb1be79542826b36f
MasaGt <gachapin2236tx@gmail.com>
2023-12-06 21:53:36 +1300

    rename push to add and fix test cases

--- List/linkedList.ts
+++ List/linkedList.ts
@@ -4,56 +4,59 @@
  *  head: ListNode<T>
  */
 class LinkedList<T> {
+  private size: number = 0;
+  private head: ListNode<T> | undefined;
+  private tail: ListNode<T> | undefined;
 
-    private head: ListNode<T> | undefined;
+  constructor();
+  constructor(values?: T[]);
+  constructor(values?: T[]) {
+    if (values === undefined || values === null) {
+      //create empty list
+      this.head = undefined;
+      this.tail = undefined;
+    } else {
+      //create non-empty list
+      values.forEach((value) => {
+        this.add(value);
+      });
+    }
+  }
 
-    constructor(values?: T[]) {
-        if(
-            values === undefined ||
-            values === null ||
-            values.length > 0
-        ) {
-            //add ListNodes based on the params
-            values?.forEach((val) => {
-                this.push(val);
-            });
-        }
+  //insert node at the end of the list
+  add(value: T): void {
+    if (this.head == undefined) {
+      //add node to the head
+      this.head = new ListNode<T>(value);
+      this.tail = this.head;
+    } else {
+      //add node to the tail
+      const newNode = new ListNode<T>(value);
+      if (this.tail) {
+        this.tail.next = newNode;
+        this.tail = newNode;
+      }
+    }
+    this.size++;
+  }
+
+  toString(): string {
+    let contents: string = "";
+
+    let currentNode: ListNode<T> | undefined = this.head;
+
+    while (currentNode) {
+      // add ", " between the nodes
+      if (!currentNode.next) {
+        contents += currentNode.value;
+      } else {
+        contents += currentNode.value + ", ";
+      }
+      currentNode = currentNode.next;
     }
 
-    //insert node at the end of the list
-    push(value: T): void {
-        if (this.head == undefined) {
-            //add node to the head
-            this.head = new ListNode<T>(value);
-            return;
-        } else {
-            
-            let currentNode = this.head;
-            while(currentNode.next) {
-                currentNode = currentNode.next;
-            }
-
-            currentNode.next = new ListNode<T>(value);
-        }
-    }
-
-    toString(): string {
-        let contents: string = '';
-        
-        let currentNode: ListNode<T> | undefined= this.head;
-
-        while (currentNode) {
-            // add ", " between the nodes
-            if (!currentNode.next) {
-                contents += currentNode.value;
-            } else {
-                contents += currentNode.value + ', ';
-            }
-            currentNode = currentNode.next;
-        }
-
-        return contents;
-    }
+    return contents;
+  }
 }
 
 /**
@@ -62,25 +65,27 @@ class LinkedList<T> {
  *  value: data
  *  next: pointer to the next ListNode
  */
-class ListNode<T>{
+class ListNode<T> {
+  constructor(
+    private _value: T,
+    private _next?: ListNode<T>
+  ) {}
 
-    constructor(private _value: T, private _next?: ListNode<T>) {}
+  get value(): T {
+    return this._value;
+  }
 
-    get value():T {
-        return this._value;
-    }
+  set value(value: T) {
+    this._value = value;
+  }
 
-    set value(value: T) {
-        this._value = value;
-    }
+  get next(): ListNode<T> | undefined {
+    return this._next;
+  }
 
-    get next(): ListNode<T> | undefined {
-        return this._next;
-    }
-
-    set next(next: ListNode<T>) {
-        this._next = next;
-    }
+  set next(next: ListNode<T>) {
+    this._next = next;
+  }
 }
 
 export { LinkedList, ListNode };
